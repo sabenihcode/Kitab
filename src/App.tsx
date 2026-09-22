8c5f3bd8c25b5f167d7c308d0becd68e3a1eba15
sabndev <sabenih726@gmail.com>
2026-09-22 13:00:35 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BabDetailPage } from './pages/BabDetailPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { SearchPage } from '@pages/SearchPage';
 import { TanyaAIPage } from './pages/TanyaAIPage';
+import { QuizPage } from './pages/QuizPage';
 import { BottomNav } from './components/layout/BottomNav';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/bab/:id" element={<BabDetailPage />} />
+        <Route path="/bab/:id/quiz/:paragrafId" element={<QuizPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/tanya" element={<TanyaAIPage />} />
